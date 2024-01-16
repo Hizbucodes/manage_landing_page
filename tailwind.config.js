c37a72541dee36646e549ca66c638cba77f5d288
Hizbucodes <106305260+Hizbucodes@users.noreply.github.com>
2024-01-16 20:50:08 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '780px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)'
+      }
+    },
+  },
+  plugins: [],
+}
